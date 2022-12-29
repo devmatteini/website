f5b6a097b6be0d16fbd3bd3432b3f61ecdedb6b8
Cosimo Matteini <dev.matteini@gmail.com>
2022-12-29 22:58:23 +0100

    setup MDXProvider for custom components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
+import type { MDXComponents } from "mdx/types"
+import { MDXProvider } from "@mdx-js/react"
+
+const components: MDXComponents = {}
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <MDXProvider components={components}>
+            <Component {...pageProps} />
+        </MDXProvider>
+    )
 }
