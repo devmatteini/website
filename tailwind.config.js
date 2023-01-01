91ec2a671ec86088182a1a4a9e30a9d585a021b0
Cosimo Matteini <dev.matteini@gmail.com>
2023-01-01 17:40:05 +0100

    tailwind: add posts directory to content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/pages/**/*.{ts,tsx,mdx}"],
+    content: ["./src/pages/**/*.{ts,tsx}", "./posts/**/*.mdx"],
     theme: {
         extend: {},
     },
