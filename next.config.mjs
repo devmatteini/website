97e933ef5672f483abd9f071092be5ce2303598c
Cosimo Matteini <dev.matteini@gmail.com>
2023-01-01 16:53:45 +0100

    next: set webpack infrastructureLogging level to error to remove some warning due to using next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ import { withContentlayer } from "next-contentlayer"
 /** @type {import('next').NextConfig} */
 const nextConfig = withContentlayer({
     reactStrictMode: true,
+    webpack: (config) => {
+        // https://github.com/contentlayerdev/contentlayer/issues/313
+        config.infrastructureLogging = {
+            level: "error",
+        }
+
+        return config
+    },
 })
 
 export default nextConfig
