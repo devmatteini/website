a905b0769b79d4d02b647d43e6e88eca33346668
Cosimo Matteini <dev.matteini@gmail.com>
2023-01-02 22:46:35 +0100

    styles: add custom syntax highlighting styles for code blocks

--- config/pretty-code.ts
+++ config/pretty-code.ts
@@ -3,4 +3,7 @@ import type { Options } from "rehype-pretty-code"
 
 export const prettyCodeOptions: Partial<Options> = {
     theme: "github-dark",
+    onVisitHighlightedLine(node) {
+        node.properties.className.push("line--highlighted")
+    },
 }
