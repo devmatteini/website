e32492a6f84ec2f0222b4316cde6f8b089acbb5a
Cosimo Matteini <dev.matteini@gmail.com>
2023-01-03 23:43:23 +0100

    blog: show '#' before heading link

--- config/rehype-autolink-headings.ts
+++ config/rehype-autolink-headings.ts
@@ -1,8 +1,12 @@
 import type { Options } from "rehype-autolink-headings"
 
+const removeBorder = "border-0"
+const showHashOnHover =
+    "before:content-['#'] before:absolute before:-ml-6 before:text-transparent hover:before:text-orange"
+
 export const autoLinkHeadingOptions: Partial<Options> = {
     behavior: "wrap",
     properties: {
-        className: "border-0",
+        className: `${removeBorder} ${showHashOnHover}`,
     },
 }
