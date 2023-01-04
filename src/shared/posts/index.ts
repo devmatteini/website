723147e95112fd6f907f62166411eea5c13071f7
Cosimo Matteini <dev.matteini@gmail.com>
2023-01-05 00:27:59 +0100

    shared: create posts module as contentlayer wrapper

---
+++ src/shared/posts/index.ts
@@ -0,0 +1,9 @@
+import { allPosts as generatedPosts, Post } from "contentlayer/generated"
+
+// TODO: add field to Post with Date type instead of just string
+const byMostRecentDate = (a: Post, b: Post): number => Date.parse(b.date) - Date.parse(a.date)
+
+export const allPosts = generatedPosts.sort(byMostRecentDate)
+
+export const firstN = (n: number) => (posts: Post[]) => posts.slice(0, n)
+export const first3 = firstN(3)
