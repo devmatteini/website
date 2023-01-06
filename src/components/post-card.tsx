aa8a172c39e4626af77bb5cb137e98eb082e7aef
Cosimo Matteini <dev.matteini@gmail.com>
2023-01-06 17:00:08 +0100

    posts: show tags on PostCard

--- src/components/post-card.tsx
+++ src/components/post-card.tsx
@@ -23,10 +23,24 @@ const PostCard: React.FC<PostCardProps> = ({ post }) => {
                     </h4>
                     <p className="mb-0">{formatDate(post.date)}</p>
                     <p className="mt-2 break-words">{ellipse(post.description, 100)}</p>
+                    <Tags tags={post.tags} />
                 </div>
             </div>
         </Link>
     )
 }
 
+type TagsProps = { tags: Post["tags"] }
+const Tags: React.FC<TagsProps> = ({ tags }) => {
+    if (!tags.length) return null
+    return (
+        <div className="flex gap-2 items-center">
+            {tags.map((x, i) => (
+                <span key={i} className="text-sm py-1 px-2 rounded-md bg-darkBlue">
+                    {x}
+                </span>
+            ))}
+        </div>
+    )
+}
 export default PostCard
