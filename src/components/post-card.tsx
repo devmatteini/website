2394aa8bcba9fd0347b91e10eb97ec15807d33d4
Cosimo Matteini <dev.matteini@gmail.com>
2023-01-06 15:49:07 +0100

    posts: filter generated posts by their status
    In local every draft post is present

--- src/components/post-card.tsx
+++ src/components/post-card.tsx
@@ -2,6 +2,7 @@ import Link from "next/link"
 import React from "react"
 import { formatDate } from "../shared/helpers/date"
 import { Post } from "../shared/posts"
+import Optional from "./optional"
 
 type PostCardProps = {
     post: Post
@@ -13,7 +14,12 @@ const PostCard: React.FC<PostCardProps> = ({ post }) => {
             {/* This div is needed to create the effect of gradient border for the inner div */}
             <div className="mb-6 rounded p-1 bg-gradient-to-r from-[#0038a6] to-[#c60c30]">
                 <div className="p-3 bg-black rounded">
-                    <h4 className="text-xl my-0 font-bold">{post.title}</h4>
+                    <h4 className="text-xl my-0 font-bold">
+                        {post.title}
+                        <Optional condition={post.status === "draft"}>
+                            <span className="ml-2">[DRAFT]</span>
+                        </Optional>
+                    </h4>
                     <p className="mb-0">{formatDate(post.date)}</p>
                 </div>
             </div>
--- src/shared/posts/index.ts
+++ src/shared/posts/index.ts
@@ -2,10 +2,15 @@ import { allPosts as generatedPosts, Post as ContentLayerPost } from "contentlay
 
 export type Post = ContentLayerPost
 
-// TODO: add field to Post with Date type instead of just string
 const byMostRecentDate = (a: Post, b: Post): number => Date.parse(b.date) - Date.parse(a.date)
 
-export const allPosts = generatedPosts.sort(byMostRecentDate)
+const isDev = process.env.NODE_ENV === "development"
+const readyToPublish = (isDev: boolean) => (x: Post) => {
+    if (isDev) return true
+    return x.status === "published"
+}
+
+export const allPosts = generatedPosts.filter(readyToPublish(isDev)).sort(byMostRecentDate)
 
 export const firstN = (n: number) => (posts: Post[]) => posts.slice(0, n)
 export const first3 = firstN(3)
