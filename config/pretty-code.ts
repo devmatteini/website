586b3aa8f002338f50a4906a30a2d02a6f49d426
Cosimo Matteini <dev.matteini@gmail.com>
2023-03-30 22:27:27 +0200

    config - pretty-code: fix empty lines being rendered with no content

--- config/pretty-code.ts
+++ config/pretty-code.ts
@@ -6,4 +6,10 @@ export const prettyCodeOptions: Partial<Options> = {
     onVisitHighlightedLine(node) {
         node.properties.className.push("line--highlighted")
     },
+    onVisitLine(node) {
+        // Prevent lines from collapsing in `display: grid` mode, and allow empty lines to be copy/pasted
+        if (node.children.length === 0) {
+            node.children = [{ type: "text", value: " " }]
+        }
+    },
 }
