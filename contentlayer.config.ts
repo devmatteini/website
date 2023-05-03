eb03b4b7697274735b59fac6b02ac5780b5e3b5a
Cosimo Matteini <dev.matteini@gmail.com>
2023-05-04 01:16:18 +0200

    blog: extract functions to clarify how things works

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -7,6 +7,41 @@ import { documentTypes } from "./config/contentlayer"
 import { prettyCodeOptions } from "./config/pretty-code"
 import { visit } from "unist-util-visit"
 
+const extractContentFromCodeBlocks = (tree: any) => {
+    visit(tree, (node) => {
+        if (node?.type === "element" && node?.tagName === "pre") {
+            const [codeEl] = node.children
+
+            if (codeEl.tagName !== "code") return
+
+            node.__rawContent__ = codeEl.children?.[0].value
+        }
+    })
+}
+
+const injectCodeBlocksWithContentAfterSyntaxHighlight = (tree: any) => {
+    visit(tree, (node) => {
+        if (node?.type === "element" && node?.tagName === "div") {
+            if (!("data-rehype-pretty-code-fragment" in node.properties)) {
+                return
+            }
+
+            for (const child of node.children) {
+                if (child.tagName === "pre") {
+                    child.properties["__rawContent__"] = node.__rawContent__
+                }
+            }
+        }
+    })
+}
+
+// TODO: can we improve the types?
+const prettyCodePlugins: [any, any, any] = [
+    () => (tree: any) => extractContentFromCodeBlocks(tree),
+    [prettyCode, prettyCodeOptions],
+    () => (tree: any) => injectCodeBlocksWithContentAfterSyntaxHighlight(tree),
+]
+
 export default makeSource({
     contentDirPath: "posts",
     documentTypes,
@@ -15,33 +50,7 @@ export default makeSource({
         rehypePlugins: [
             rehypeSlug,
             [rehypeAutolinkHeadings, autoLinkHeadingOptions],
-            () => (tree) => {
-                visit(tree, (node) => {
-                    if (node?.type === "element" && node?.tagName === "pre") {
-                        const [codeEl] = node.children
-
-                        if (codeEl.tagName !== "code") return
-
-                        node.__rawContent__ = codeEl.children?.[0].value
-                    }
-                })
-            },
-            [prettyCode, prettyCodeOptions],
-            () => (tree) => {
-                visit(tree, (node) => {
-                    if (node?.type === "element" && node?.tagName === "div") {
-                        if (!("data-rehype-pretty-code-fragment" in node.properties)) {
-                            return
-                        }
-
-                        for (const child of node.children) {
-                            if (child.tagName === "pre") {
-                                child.properties["__rawContent__"] = node.__rawContent__
-                            }
-                        }
-                    }
-                })
-            },
+            ...prettyCodePlugins,
         ],
     },
 })
