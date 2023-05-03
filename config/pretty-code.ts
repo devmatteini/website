0a81746623af2f72ef13dd0a4bdcab5b29d21826
Cosimo Matteini <dev.matteini@gmail.com>
2023-05-04 01:36:44 +0200

    blog+config: move prettyCodePlugins to config

--- config/pretty-code.ts
+++ config/pretty-code.ts
@@ -1,7 +1,11 @@
 // DOCS: https://rehype-pretty-code.netlify.app/
 import type { Options } from "rehype-pretty-code"
+import prettyCode from "rehype-pretty-code"
+import { visit } from "unist-util-visit"
 
-export const prettyCodeOptions: Partial<Options> = {
+const __rawContent__ = "__rawContent__"
+
+const prettyCodeOptions: Partial<Options> = {
     theme: "github-dark",
     onVisitHighlightedLine(node) {
         node.properties.className.push("line--highlighted")
@@ -16,3 +20,40 @@ export const prettyCodeOptions: Partial<Options> = {
         }
     },
 }
+
+const extractContentFromCodeBlocks = (tree: any) => {
+    visit(tree, (node) => {
+        if (!(isElementType(node) && isPreTag(node))) return
+
+        const [codeEl] = node.children
+        if (!isTagName(codeEl, "code")) return
+
+        node[__rawContent__] = codeEl.children?.[0].value
+    })
+}
+
+const injectCodeBlocksWithContentAfterSyntaxHighlight = (tree: any) => {
+    visit(tree, (node) => {
+        if (!(isElementType(node) && isDivTag(node))) return
+        if (!("data-rehype-pretty-code-fragment" in node.properties)) return
+
+        for (const child of node.children) {
+            if (isPreTag(child)) {
+                child.properties[__rawContent__] = node[__rawContent__]
+            }
+        }
+    })
+}
+
+// TODO: can we improve the types?
+export const prettyCodePlugins: [any, any, any] = [
+    () => (tree: any) => extractContentFromCodeBlocks(tree),
+    [prettyCode, prettyCodeOptions],
+    () => (tree: any) => injectCodeBlocksWithContentAfterSyntaxHighlight(tree),
+]
+
+const isType = (node: any, expectedType: string) => node?.type === expectedType
+const isElementType = (node: any) => isType(node, "element")
+const isTagName = (node: any, expectedTagName: string) => node?.tagName === expectedTagName
+const isPreTag = (node: any) => isTagName(node, "pre")
+const isDivTag = (node: any) => isTagName(node, "div")
--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,50 +1,9 @@
 import { autoLinkHeadingOptions } from "./config/rehype-autolink-headings"
 import { makeSource } from "contentlayer/source-files"
 import rehypeAutolinkHeadings from "rehype-autolink-headings"
-import prettyCode from "rehype-pretty-code"
 import rehypeSlug from "rehype-slug"
 import { documentTypes } from "./config/contentlayer"
-import { prettyCodeOptions } from "./config/pretty-code"
-import { visit } from "unist-util-visit"
-
-const __rawContent__ = "__rawContent__"
-
-const isType = (node: any, expectedType: string) => node?.type === expectedType
-const isElementType = (node: any) => isType(node, "element")
-const isTagName = (node: any, expectedTagName: string) => node?.tagName === expectedTagName
-const isPreTag = (node: any) => isTagName(node, "pre")
-const isDivTag = (node: any) => isTagName(node, "div")
-
-const extractContentFromCodeBlocks = (tree: any) => {
-    visit(tree, (node) => {
-        if (!(isElementType(node) && isPreTag(node))) return
-
-        const [codeEl] = node.children
-        if (!isTagName(codeEl, "code")) return
-
-        node[__rawContent__] = codeEl.children?.[0].value
-    })
-}
-
-const injectCodeBlocksWithContentAfterSyntaxHighlight = (tree: any) => {
-    visit(tree, (node) => {
-        if (!(isElementType(node) && isDivTag(node))) return
-        if (!("data-rehype-pretty-code-fragment" in node.properties)) return
-
-        for (const child of node.children) {
-            if (isPreTag(child)) {
-                child.properties[__rawContent__] = node[__rawContent__]
-            }
-        }
-    })
-}
-
-// TODO: can we improve the types?
-const prettyCodePlugins: [any, any, any] = [
-    () => (tree: any) => extractContentFromCodeBlocks(tree),
-    [prettyCode, prettyCodeOptions],
-    () => (tree: any) => injectCodeBlocksWithContentAfterSyntaxHighlight(tree),
-]
+import { prettyCodePlugins } from "./config/pretty-code"
 
 export default makeSource({
     contentDirPath: "posts",
