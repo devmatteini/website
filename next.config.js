6b343c492e702865e88fc159282742184c05fefd
Cosimo Matteini <dev.matteini@gmail.com>
2023-05-11 01:27:16 +0200

    update nextjs to 13.4.1 for stable appDir

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const { withContentlayer } = require("next-contentlayer")
 /** @type {import('next').NextConfig} */
 const nextConfig = withContentlayer({
     reactStrictMode: true,
-    experimental: {
-        appDir: true,
-    },
 })
 
 module.exports = nextConfig
