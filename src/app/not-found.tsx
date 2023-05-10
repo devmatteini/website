9fec516813a97db4d382ad9a90d7f7b8bd27bc5e
Cosimo Matteini <dev.matteini@gmail.com>
2023-05-11 00:22:35 +0200

    app: migrate 404 -> not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,14 @@
-import Head from "next/head"
+import type { Metadata } from "next"
 import NextLink from "next/link"
 import React from "react"
+
+export const metadata: Metadata = {
+    title: "404 - Page not found",
+}
 
 const NotFound: React.FC = () => {
     return (
         <>
-            <Head>
-                <title>404 - Page not found</title>
-            </Head>
             <h1 className="text-3xl md:text-5xl font-bold text-center">404 - Page not found</h1>
             <p className="text-center">
                 It seems you&apos;ve found something that used to exist, or you spelled something
