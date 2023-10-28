00f4ed04017fdfc5ae97711f1a4b3bede254a8a5
Cosimo Matteini <dev.matteini@gmail.com>
2023-10-28 17:22:19 +0200

    nextjs: skip eslint and typecheck during build since it's already done

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const { withContentlayer } = require("next-contentlayer")
 /** @type {import('next').NextConfig} */
 const nextConfig = withContentlayer({
     reactStrictMode: true,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
 })
 
 module.exports = nextConfig
