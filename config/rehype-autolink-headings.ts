15c5d76694c9a8c17b4996b3188d61ad6b5d4ee3
Cosimo Matteini <dev.matteini@gmail.com>
2023-11-08 21:51:39 +0100

    rehype-autolink-headings: fix '#' on headings on smaller devices

--- config/rehype-autolink-headings.ts
+++ config/rehype-autolink-headings.ts
@@ -2,7 +2,7 @@ import type { Options } from "rehype-autolink-headings"
 
 const removeBorder = "border-0"
 const showHashOnHover =
-    "before:content-['#'] before:absolute before:-ml-6 before:text-transparent hover:before:text-orange2"
+    "after:content-['#'] after:absolute after:text-transparent hover:after:text-orange2"
 
 export const autoLinkHeadingOptions: Partial<Options> = {
     behavior: "wrap",
