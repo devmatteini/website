a50bb9c697ae3a7c4c2e4dcb8cb215692e47f9b3
Cosimo Matteini <dev.matteini@gmail.com>
2023-12-30 22:13:38 +0100

    v2: setup Inter font

--- v2/tailwind.config.mjs
+++ v2/tailwind.config.mjs
@@ -1,8 +1,13 @@
+import defaultTheme from "tailwindcss/defaultTheme"
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
         extend: {
+            fontFamily: {
+                sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
+            },
             colors: {
                 black: "#202229",
                 darkBlue: "#18181e",
