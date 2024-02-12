954d5907f4b511278a53953235d3e2f8bb0ee748
Cosimo Matteini <dev.matteini@gmail.com>
2024-02-13 00:33:42 +0100

    metadata: fix wrong port number on local environment

--- src/shared/metadata.ts
+++ src/shared/metadata.ts
@@ -1,5 +1,5 @@
 export const websiteDomain = "cosimomatteini.com"
 
-const baseUrl = () => (import.meta.env.DEV ? `http://localhost:3000` : `https://${websiteDomain}`)
+const baseUrl = () => (import.meta.env.DEV ? `http://localhost:4321` : `https://${websiteDomain}`)
 
 export const websiteUrl = (route: string) => `${baseUrl()}${route}`
