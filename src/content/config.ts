e2e60aca4e6ae193ec8df71c0fc855b101d77db8
Cosimo Matteini <dev.matteini@gmail.com>
2024-04-26 16:42:56 +0200

    content: add refine to make sure updatedOn is greater than date

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,15 +2,25 @@ import { defineCollection, z } from "astro:content"
 
 const posts = defineCollection({
     type: "content",
-    schema: z.object({
-        title: z.string(),
-        date: z.date(),
-        updatedOn: z.date().optional(),
-        status: z.enum(["draft", "published"]),
-        description: z.string(),
-        tags: z.array(z.string()).default([]),
-        seoKeywords: z.array(z.string()).default([]),
-    }),
+    schema: z
+        .object({
+            title: z.string(),
+            date: z.date(),
+            updatedOn: z.date().optional(),
+            status: z.enum(["draft", "published"]),
+            description: z.string(),
+            tags: z.array(z.string()).default([]),
+            seoKeywords: z.array(z.string()).default([]),
+        })
+        .superRefine((data, ctx) => {
+            if (data.updatedOn && data.updatedOn <= data.date) {
+                ctx.addIssue({
+                    code: z.ZodIssueCode.invalid_date,
+                    path: ["updatedOn"],
+                    message: `updatedOn (${data.updatedOn.toISOString()}) must be greater than date (${data.date.toISOString()})`,
+                })
+            }
+        }),
 })
 
 export const collections = {
