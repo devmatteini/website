afdc6a5466d4ec8bf62e264cf50fc51c0c11431a
Cosimo Matteini <dev.matteini@gmail.com>
2024-05-04 16:03:44 +0200

    mdx: remove now unnecessary code for code blocks row content

--- config/pretty-code.ts
+++ config/pretty-code.ts
@@ -1,10 +1,6 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 // DOCS: https://rehype-pretty-code.netlify.app/
 import type { Options } from "rehype-pretty-code"
 import prettyCode from "rehype-pretty-code"
-import { visit } from "unist-util-visit"
-
-const __rawContent__ = "__rawContent__"
 
 const prettyCodeOptions: Partial<Options> = {
     theme: "github-dark",
@@ -14,40 +10,4 @@ const prettyCodeOptions: Partial<Options> = {
     },
 }
 
-const extractContentFromCodeBlocks = (tree: any) => {
-    visit(tree, (node) => {
-        if (!(isElementType(node) && isPreTag(node))) return
-
-        const [codeEl] = node.children
-        if (!isCodeTag(codeEl)) return
-
-        node[__rawContent__] = codeEl.children?.[0].value
-    })
-}
-
-const injectCodeBlocksWithContentAfterSyntaxHighlight = (tree: any) => {
-    visit(tree, (node) => {
-        if (!(isElementType(node) && isFigureTag(node))) return
-        if (!("data-rehype-pretty-code-figure" in node.properties)) return
-
-        for (const child of node.children) {
-            if (isPreTag(child)) {
-                child.properties[__rawContent__] = node[__rawContent__]
-            }
-        }
-    })
-}
-
-// TODO: can we improve the types?
-export const prettyCodePlugins: [any, any, any] = [
-    () => (tree: any) => extractContentFromCodeBlocks(tree),
-    [prettyCode, prettyCodeOptions],
-    () => (tree: any) => injectCodeBlocksWithContentAfterSyntaxHighlight(tree),
-]
-
-const isType = (node: any, expectedType: string) => node?.type === expectedType
-const isElementType = (node: any) => isType(node, "element")
-const isTagName = (node: any, expectedTagName: string) => node?.tagName === expectedTagName
-const isPreTag = (node: any) => isTagName(node, "pre")
-const isFigureTag = (node: any) => isTagName(node, "figure")
-const isCodeTag = (node: any) => isTagName(node, "code")
+export const prettyCodePlugins = [[prettyCode, prettyCodeOptions]]
