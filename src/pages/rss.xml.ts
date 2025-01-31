b340ffd07760fb6685285641c4c991e97ddacbf0
Cosimo Matteini <dev.matteini@gmail.com>
2025-01-31 23:06:28 +0100

    add rss feed page

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,21 @@
+import rss from "@astrojs/rss"
+import { publishedPosts } from "@shared/blog"
+import type { APIRoute } from "astro"
+
+export const GET: APIRoute = async (context) => {
+    const blog = await publishedPosts()
+    const site = context.site ?? new URL("https://cosimomatteini.com")
+
+    return rss({
+        title: "Cosimo Matteini's Blog",
+        description: "",
+        site,
+        items: blog.map((post) => ({
+            title: post.data.title,
+            pubDate: post.data.updatedOn ?? post.data.date,
+            description: post.data.description,
+            link: post.data.url,
+        })),
+        customData: "<language>en-us</language>",
+    })
+}
