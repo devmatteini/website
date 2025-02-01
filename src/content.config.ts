deade5c053386a17cced3f70031b643188b1a4c9
Cosimo Matteini <dev.matteini@gmail.com>
2025-02-01 16:55:08 +0100

    blog: create zod type for tag to always be lowercase and not contain whitespace

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,19 @@
 import { glob } from "astro/loaders"
 import { defineCollection, z } from "astro:content"
 
+const Tag = z
+    .string()
+    .toLowerCase()
+    .superRefine((data, ctx) => {
+        if (/\s/.test(data)) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                path: ctx.path,
+                message: `Tag must not contain whitespaces: ${data}`,
+            })
+        }
+    })
+
 const blog = defineCollection({
     // NOTE: base is relative to the project root
     loader: glob({ base: "./blog", pattern: "**/*.{md,mdx}" }),
@@ -11,7 +24,7 @@ const blog = defineCollection({
             updatedOn: z.date().optional(),
             status: z.enum(["draft", "published"]),
             description: z.string(),
-            tags: z.array(z.string()).default([]),
+            tags: z.array(Tag).default([]),
             seoKeywords: z.array(z.string()).default([]),
             tableOfContents: z.boolean().default(false),
         })
