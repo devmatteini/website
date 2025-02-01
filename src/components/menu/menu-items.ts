9b1922beb24913312a97856e1a783e273343099e
Cosimo Matteini <dev.matteini@gmail.com>
2025-02-01 18:46:20 +0100

    first iteration for new responsive navbar navigation

---
+++ src/components/menu/menu-items.ts
@@ -0,0 +1,31 @@
+export type MenuItem =
+    | {
+          type: "page"
+          route: string
+          label: string
+      }
+    | {
+          type: "social"
+          icon: string
+          link: string
+          label: string
+      }
+
+export const menuItems: readonly MenuItem[] = [
+    {
+        type: "page",
+        route: "/blog",
+        label: "/blog",
+    },
+    {
+        type: "page",
+        route: "/tags",
+        label: "/tags",
+    },
+    {
+        type: "social",
+        icon: "github",
+        link: "https://github.com/devmatteini/",
+        label: "GitHub",
+    },
+]
