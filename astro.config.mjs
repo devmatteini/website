60bf7ae03a61eb1ea6a0b89bacb8aaf081614cd7
Cosimo Matteini <dev.matteini@gmail.com>
2026-03-23 21:51:40 +0100

    remove vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings"
 import { autoLinkHeadingOptions } from "./config/rehype-autolink-headings"
 import rehypeSlug from "rehype-slug"
 import { prettyCodePlugins } from "./config/pretty-code"
-import { isAnalyticsEnabled } from "./config/analytics"
 import vercel from "@astrojs/vercel"
 import sitemap from "@astrojs/sitemap"
 import { fixAstroVercelSitemap } from "./fix-astro-vercel-sitemap"
@@ -25,9 +24,5 @@ export default defineConfig({
     },
     integrations: [tailwind(), icon(), mdx(), sitemap(), fixAstroVercelSitemap()],
     output: "static",
-    adapter: vercel({
-        webAnalytics: {
-            enabled: isAnalyticsEnabled,
-        },
-    }),
+    adapter: vercel(),
 })
--- config/analytics.ts
+++
@@ -1,3 +0,0 @@
-export const isAnalyticsEnabled = import.meta.env.DEV
-    ? process.env.ANALYTICS_DEBUG === "true"
-    : true
